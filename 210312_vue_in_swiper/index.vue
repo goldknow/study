a5eb2780d790a095d5597c9c2d62d0adb9ee34b2
김아라 <goldknow456@gmail.com>
2021-03-12 10:53:29 +0900

    vue cli 에서 swiper 사용하기

---
+++ 210312_vue_in_swiper/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <list-item>리스트 아이템</list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from "@/경로";
+export default {
+  name: "index",
+  components: {ListItem}
+}
+</script>
---
+++ 210312_vue_in_swiper/listItem.vue
@@ -0,0 +1,74 @@
+<template>
+	<swiper class="swiper" ref="mySwiper" 
+		:options="swiperOptions" 
+		@slideChange="slideChangeTransitionStart"
+	>
+
+		<swiper-slide>1</swiper-slide>
+		<swiper-slide>2</swiper-slide>
+		<swiper-slide>3</swiper-slide>
+
+		<div class="swiper-pagination" slot="pagination"></div>
+	</swiper>
+</template>
+
+
+<script>
+	import Vue from 'vue'
+	import VueAwesomeSwiper from 'vue-awesome-swiper'
+	import 'swiper/css/swiper.css'
+	Vue.use(VueAwesomeSwiper);
+
+	import {Swiper,SwiperSlide	} from 'vue-awesome-swiper'
+
+	export default {
+		name: 'ListItem',
+		components: {
+			Swiper,
+			SwiperSlide
+		},
+		methods: {
+			slideChangeTransitionStart() {
+				console.log(this.swiper.activeIndex);
+			}
+		},
+		data() {
+			return {
+				swiperOptions: {
+					//loop: true,
+					// autoplay:{
+					//   delay:1000
+					// },
+					pagination: {
+						el: '.swiper-pagination'
+					}
+				},
+			}
+		},
+
+		//참조하고 있는 값이 변경될 때마다 정의된 계산식에 따라 값을 출력
+		computed: {
+			swiper() {
+				return this.$refs.mySwiper.$swiper;
+			}
+		},
+
+		//el이 새로 생성된 vm.$el로 인스턴스가 마운트 된 직후
+		mounted() {
+			//console.log('Current Swiper instance object', this.swiper)
+			//this.swiper.slideTo(3, 1000, false)
+		}
+
+
+	}
+</script>
+
+<style lang="scss" scoped>
+	.swiper-slide {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-weight: bold;
+	}
+</style>
